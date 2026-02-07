2c58e1b3bd85c0d342b92e32b896d69c00dd779e
Fauzansyz <fauzanadzma2@gmail.com>
2026-02-07 17:25:26 +0000

    Optimizing SEO & refactoring UI

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -4,15 +4,15 @@ export default function AboutPage() {
       <h2 className="text-3xl font-bold text-center">About me</h2>
 
       <div className="mt-6 px-5 md:px-2 space-y-5 text-zinc-600 dark:text-zinc-400 leading-relaxed">
-        <p>
+        <p className="text-center">
           I like building projects from scratch, starting with determining the architecture until the project is successful  <b>deployed</b>
         </p>
 
-        <p>
+        <p className="text-center">
           And for me, a structured project is better and more expensive than a project that is running but is not organized.
         </p>
 
-        <p>
+        <p className="text-center">
           I prefer to prioritize the functionality and scalability of applications that can be useful for the development of applications that I work on in the future. </p>
 
         <p className="font-medium text-zinc-800 dark:text-zinc-200">
