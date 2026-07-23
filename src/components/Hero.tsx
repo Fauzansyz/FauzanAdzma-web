a2921db22b9688595e9681b012db1e54cd334685
Fauzansyz <fauzanadzma2@gmail.com>
2026-07-23 16:38:24 +0700

    Rev hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,22 +12,21 @@ export default function Hero({ onScroll }: HeroSection) {
 			<div className={`${theme === "dark" ? "hero-gradient" : "hero-gradient-light"} absolute inset-0 p-20`}></div>
 
 			<div className="relative z-10 max-w-3xl">
+				<h1 className="mb-5 scroll-m-20 text-center text-6xl lg:text-4xl md:text-6xl font-extrabold tracking-tight text-balance">
+					{S_H.a}<TextType
+						text={[`${S_H.n}`, `${S_H.p}`]}
+						typingSpeed={75}
+						showCursor={false}
+						startOnVisible={true}
+					/>
+				</h1>
+
 				<div className="max-w-3xl flex gap-4 justify-center">
 
 					<Badge className="mb-4">Web Developer</Badge>
 					<Badge className="mb-4">Mobile Developer</Badge>
 
 				</div>
-
-				<h1 className="mb-5 scroll-m-20 text-center text-6xl lg:text-4xl md:text-6xl font-extrabold tracking-tight text-balance">
-					{S_H.a}<TextType
-						text={[`${S_H.n}`, `${S_H.p}`]}
-						typingSpeed={75}
-						loop={true}
-						showCursor={false}
-						startOnVisible={true}
-					/>
-				</h1>
 
 				<p className="mb-8 leading-7 [&:not(:first-child)]:text-muted-foreground">
 					{S_H.si}
