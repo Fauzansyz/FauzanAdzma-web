1391ab08759b2442d1f74ac22bff48242e48d740
Fauzansyz <fauzanadzma2@gmail.com>
2026-07-23 20:38:51 +0700

    Rev hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <nav className="container mx-auto px-6 py-6 lg:px-16 flex justify-between items-center relative z-20">
 
         {/* Desktop Menu */}
-        <div className="hidden flex space-x-8 text-xs font-semibold tracking-widest text-gray-500 uppercase">
+        <div className="flex space-x-8 text-xs font-semibold tracking-widest text-gray-500 uppercase">
           <a href="#home" className="text-red-500">Home</a>
           <a href="#about" className="hover:text-red-500 transition">About</a>
           <a href="#service" className="hover:text-red-500 transition">Service</a>
