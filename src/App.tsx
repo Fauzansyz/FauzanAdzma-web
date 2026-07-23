7e53c07e1702154eb1c034a01cc662a71addf760
Fauzansyz <fauzanadzma2@gmail.com>
2026-07-23 20:34:51 +0700

    Rev hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import { useRef } from 'react'
 import ProjectsSection from '@/components/ProjectsSection'
