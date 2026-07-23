b1bed8874c9f4a6ea896cab5143a5d17f06a1e65
Fauzansyz <fauzanadzma2@gmail.com>
2026-07-23 22:17:09 +0700

    Rev contact section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,10 @@ export default function Navbar() {
       <nav className="container mx-auto px-6 py-6 lg:px-16 flex justify-between items-center relative z-20">
 
         {/* Desktop Menu */}
-        <div className="flex items-center space-x-8 text-xs font-semibold tracking-widest text-gray-500 uppercase">
+        <div className="flex items-center space-x-8 text-xs font-semibold justify-between tracking-widest text-gray-500 uppercase">
           <a href="#home" className="text-red-500">Home</a>
           <a href="#about" className="hover:text-red-500 transition">About</a>
-          <a href="#service" className="hover:text-red-500 transition">Service</a>
-          <a href="#portfolio" className="hover:text-red-500 transition">Portfolio</a>
+          <a href="#service" className="hover:text-red-500 transition">Projects</a>
         </div>
       </nav>
       </>
