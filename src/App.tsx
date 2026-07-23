8d6fdca4e8f0bb63c29b8d340d0caa68482d95fb
Fauzansyz <fauzanadzma2@gmail.com>
2026-07-23 20:45:24 +0700

    Rev hero section

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="h-full w-full">
+      <div className="h-full w-full bg-[#F8FAFC]">
         <Hero />
         <div className='mx-auto max-w-6xl px-8 py-24'>
           <SkillStackSection />
