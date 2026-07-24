55b872d0813d1ce27f86ff194e5cd6cb85c1db15
Fauzansyz <fauzanadzma2@gmail.com>
2026-07-24 15:07:29 +0700

    Rev navbar section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
     <>
-      <nav className="container mx-auto py-6 lg:px-16 flex justify-between space-x-5 items-center relative z-20">
+      <nav className="container mx-auto py-6 lg:px-16 flex items-center relative z-20">
 
-        <div className="flex justify-evenly text-xs font-semibold tracking-widest text-gray-500 uppercase">
+        <div className="flex justify-between text-xs font-semibold tracking-widest text-gray-500 uppercase">
           
           <a href="#portfolio" className="hover:text-red-500 transition">Portfolio</a>
           <a href="#home" className="text-red-500">Home</a>
